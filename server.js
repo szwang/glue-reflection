3bce1802868dc3d89d07724ee8851eeb5584ae13
Suzanne Wang <suzanneswang@gmail.com>
2016-01-10 10:37:30 -0500

    [feat] add /img post method

--- server.js
+++ server.js
@@ -7,27 +7,43 @@ var config = require('./webpack.config');
 var AWS = require('aws-sdk');
 var app = express();
 
+
+var compiler = webpack(config);
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+}));
+app.use(require('webpack-hot-middleware')(compiler));
+
 app.use(function(req, res, next) {
   console.log(req.path);
   next();
 });
 
-var compiler = webpack(config);
-
-app.use(require('webpack-dev-middleware')(compiler, {
-    noInfo: true,
-    publicPath: config.output.publicPath
-}));
-
-app.use(require('webpack-hot-middleware')(compiler));
-
 app.use(bodyParser.json());
+
+AWS.config.loadFromPath('./s3_config.json');
+var s3Bucket = new AWS.s3({ params: {Bucket: 'glue-photos'} })
 
 
 /** ROUTES **/
 
 app.post('/img', function(req, res) {
-  var 
+  var url = req.body.imgURL;
+  buf = new Buffer(url.replace(/^data:image\/\w+;base64,/, ""), base64);
+  var data = {
+    Body: buf,
+    ContentEncoding: 'base64',
+    ContentType: 'image/jpeg'
+  }
+  console.log('data: ', data);
+  s3Bucket.putObject(data, function(err, data) {
+    if(err) {
+      console.log('error: ', err);
+    } else {
+      console.log('image upload success!')
+    }
+  })
 })
 
 /** SERVE **/
