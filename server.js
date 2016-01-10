3769cc7bd12a155edd36f210e53ef2b42fbec8e0
Suzanne Wang <suzanneswang@gmail.com>
2016-01-10 12:57:20 -0500

    [chore] update gitignore

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.post('/img', function(req, res) {
 /** SERVE **/
 
 app.use('/', express.static(path.join(__dirname, '/build')));
-server.use('/assets', express.static(path.join(__dirname, '/assets')));
+app.use('/assets', express.static(path.join(__dirname, '/assets')));
 
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, '/src/index.html'));
