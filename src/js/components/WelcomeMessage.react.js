1ec079a5f1ef8516bd343f43458388736d344a65
Suzanne Wang <suzanneswang@gmail.com>
2016-01-11 10:53:17 -0500

    [feat] add link to cam page from home

--- src/js/components/WelcomeMessage.react.js
+++ src/js/components/WelcomeMessage.react.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router';
 import { Button } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 class WelcomeMessage extends React.Component {
   constructor(props) {
@@ -14,7 +15,9 @@ class WelcomeMessage extends React.Component {
         Hello! As part of a study in facial expressions, you will be presented with a webcam when you click the button below. 
         Please smile, press the "Take Photo" button to take a snapshot, then submit the photo once you are done.
         </div>
-        <Button bsStyle="primary" bsSize="large" block>Go to Smile Recorder</Button>
+        <LinkContainer to={{ pathname: '/camera' }}>
+          <Button bsStyle="primary" bsSize="large" block>Go to Smile Recorder</Button>
+        </LinkContainer>
         <div>
         We will not share, sell, or distribute your image.
         </div>
