b0f0e24ccfddb29eff48e0377fa7908f9d5345a3
Suzanne Wang <suzanneswang@gmail.com>
2016-01-12 16:54:51 -0500

    [feat] build stopRecord function

--- src/js/components/Recorder.react.js
+++ src/js/components/Recorder.react.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import RecordRTC from 'recordrtc';
-import { captureUserMedia } from '../utils/RecorderUtils';
+import { captureUserMedia, onStopRecording } from '../utils/RecorderUtils';
+
+function isFirefox() {
+  return !!navigator.mozGetUserMedia;
+}
 
 class Recorder extends React.Component {
   constructor(props) {
@@ -8,30 +12,53 @@ class Recorder extends React.Component {
 
     this.state = {
       mediaStream: null,
-      src: null
+      src: null,
+      recordAudio: null,
+      recordVideo: null
     }
 
     this.startRecord = this.startRecord.bind(this);
+    this.stopRecord = this.stopRecord.bind(this);
   }
 
   startRecord() {
-    const isFirefox = !!navigator.mozGetUserMedia;
-    var recordAudio, recordVideo;
-
     captureUserMedia((stream) => {
       this.setState({ mediaStream: stream });
       this.setState({ src: window.URL.createObjectURL(stream) });
 
       //set RecordRTC object and handle browser cases
-      recordAudio = RecordRTC(stream, { bufferSize: 16384 });
-      if(!isFirefox) {
-        recordVideo = RecordRTC(stream, { type: 'video' });
+      this.state.recordAudio = RecordRTC(stream, { bufferSize: 16384 });
+      if(!isFirefox()) {
+        this.state.recordVideo = RecordRTC(stream, { type: 'video' });
       }
 
       //begin recording
-      recordAudio.startRecording();
-      if(!isFirefox) {
-        recordVideo.startRecording();
+      this.state.recordAudio.startRecording();
+      if(!isFirefox()) {
+        this.state.recordVideo.startRecording();
+      }
+    })
+  }
+
+  stopRecord() {
+    this.state.recordAudio.stopRecording(() => {
+      if(isFirefox()) {
+        onStopRecording();
+      } else {
+        this.state.recordVideo.stopRecording();
+        onStopRecording();
+      }
+    })
+  }
+
+  onStopRecording() {
+    this.state.recordAudio.getDataURL((audioDataURL) => {
+      if(isFirefox()) {
+        this.state.recordVideo.getDataURL((videoDataURL) => {
+          RecorderActionCreators.postFiles(audioDataURL, videoDataURL);
+        })
+      } else {
+        RecorderActionCreators.postFiles(audioDataURL);
       }
     })
   }
