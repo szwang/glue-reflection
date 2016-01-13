0afb7a1e189ee9f164d04556d9e2ac3906b906a7
Suzanne Wang <suzanneswang@gmail.com>
2016-01-13 16:30:32 -0500

    [feat] watchPage view layout

--- src/js/components/GlueVideo.react.js
+++ src/js/components/GlueVideo.react.js
@@ -5,7 +5,10 @@ class GlueVideo extends React.Component {
   render() {
     return (
       <div className={styles.vidContainer}>
-        <video />
+        <video autoPlay loop>
+          <source src="https://s3.amazonaws.com/recordrtc-test/sample-vids/Turkey_On_The_Run.mp4"
+                  type="video/mp4" />
+        </video>
       </div>
     )
   } 
