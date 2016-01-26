0aa92b145266107aefe5baaa1cdfd6081d352b1c
Suzanne Wang <suzanneswang@gmail.com>
2016-01-26 13:20:34 -0500

    [refactor] firebase db and database fetch

--- src/js/actions/WallActionCreators.js
+++ src/js/actions/WallActionCreators.js
@@ -1,19 +1,23 @@
 import Dispatcher from '../AppDispatcher';
 import ActionType from '../AppConstants';
+import _ from 'lodash';
 
 export default {
 
   getVideos(source) {
-    var firebaseRef = new Firebase('https://reactionwall.firebaseio.com/videos/'+ source +'/reactions');
+    var firebaseRef = new Firebase('https://reactionwall.firebaseio.com/videos/'+ source +'/selected');
     var videos = [];
     firebaseRef.orderByKey().on('child_added', (snapshot) => {
       videos.push(snapshot.val());
       // console.log('videos: ', snapshot.val(), videos.length)
       if(videos.length === 14) {
+        console.log('vid array before', videos)
+        var shuffled = _.shuffle(videos)
+        console.log('vid array after', shuffled);
         Dispatcher.dispatch({
           type: ActionType.GETTING_WALL_VIDEOS,
-          vidArray: videos
-        })
+          vidArray: shuffled
+        });
         return;
       }
     })
