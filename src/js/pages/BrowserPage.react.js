18276063e9dbf83c6020c3943257567729440b12
Suzanne Wang <suzanneswang@gmail.com>
2016-02-23 14:11:53 -0500

    [feat] add browsing components

---
+++ src/js/pages/BrowserPage.react.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import _ from 'lodash';
+// import styles from '../../'
+
+class BrowserPage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillMount() {
+
+  }
+
+  render() {
+    return (
+      <div>BrowserPage</div>
+    )
+  }
+}
+
+module.exports = BrowserPage;
