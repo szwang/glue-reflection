cda7af9608cb5c4e818561cb0621e7fddee8a236
Suzanne Wang <suzanneswang@gmail.com>
2016-03-03 11:41:02 -0500

    [feat] inject nav bar

--- src/js/App.react.js
+++ src/js/App.react.js
@@ -2,10 +2,12 @@
 import React from 'react';  
 import Home from './pages/HomePage.react';
 import styles from '../styles/global.css';
+import NavBar from './components/NavBar.react';
 
 class App extends React.Component {
   render() {
     return (
+      <NavBar />
       <div className={styles.appWrapper}>
         {this.props.children || <Home />}
       </div>
---
+++ src/js/components/NavBar.react.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import _ from 'lodash';
+import styles from '../../styles/global.css';
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  componentWillMount() {
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentwillUnmount() {
+  }
+
+  render() {
+    return (
+      <div>
+      NavBar
+      </div>
+    )
+  }
+}
+
+module.exports = NavBar;
