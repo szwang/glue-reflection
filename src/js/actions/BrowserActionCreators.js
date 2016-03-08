b261e31c1c1f4c825f21b054d01df2adf27af3af
Suzanne Wang <suzanneswang@gmail.com>
2016-03-08 13:19:36 -0500

    [style] clean console logs

--- src/js/actions/BrowserActionCreators.js
+++ src/js/actions/BrowserActionCreators.js
@@ -10,10 +10,8 @@ export default {
     firebaseRef.orderByKey().on('child_added', (snapshot) => {
       var selected = snapshot.val().selected;
       var random = _.random(selected.length - 1); 
-      console.log('querying random #' + random, snapshot.key(), snapshot.val())
       _.forEach(selected, (val, i) => {
         if(i == random) {
-          console.log("gif #" + i, val, snapshot.key())
           Dispatcher.dispatch({
             type: ActionType.GOT_GIFS,
             gif: val,
